f865626d52aea5188e6997ce93878ae492347c9a
Daniel <daniel.somji@gmail.com>
2024-11-15 15:49:00 +0100

    added how to use the app as a teacher

--- src/i18n/de.js
+++ src/i18n/de.js
@@ -49,5 +49,31 @@ export default {
         time: "Uhrzeit",
         highestBac: "Höchster Alkoholwert",
         explainer: "Klicke auf einen Bereich in dem Diagramm um zu sehen, welche Auswirkung dieser Alkoholgehalt auf dich hat."
+    },
+    teachers: {
+        title: "Für Lehrer:innen & Trainer:innen",
+        intro: {
+            title: "Für Trainer:innen und Lehrer:innen",
+            text: "Die folgende Seite soll Pädagog:innen und Sozialarbeiter:innen einen Überblick darüber geben, wie C-alcohol genutzt werden kann um mit einer Gruppe das Thema Alkohol zu bearbeiten. Ich werde hierbei aus der Ich-Perspektive schreiben und aus meiner Erfahrung als Erlebnispädagoge in der schulischen Suchtprävention in Wien schreiben."
+        },
+        neutral: {
+            title: "Wertungsfreier Zugang",
+            text1: "Ich hatte über viele Jahre das Privileg in einem Workshopsetting arbeiten zu dürfen in dem ich und meine Kolleg:innen wertungsfrei arbeiten durften. Das bedeutet, dass wir als externe Trainer:innen zumeist einmalige Workshops mit Schulklassen hatten in denen wir eine inhaltliche Schweigepflicht mit den Lehrenden vereinbart haben. Die Schüler:innen können von Erfahrungen erzählen und Fragen stellen ohne dass sie Gefahr laufen, dass dies Einfluss auf ihre Noten hat. Dies ist ein äußerst privilegierter Zugang. Nicht jeder kann sich einen solchen Workshop leisten (in Wien zum Glück teilweise gefördert) und Lehrer:innen, die selbst das Thema bearbeiten werden sich - egal wie entspannt das Verhältnis zu den Schüler:innen ist - sich immer in einem Machtverhältnis zu diesen befinden.",
+            text2: "Generell ist bei einem wertungsfreien Zugang die größte Herausforderung, zwar nicht zu bewerten, was die Schüler:innen erzählen, zugleich aber das Gesagte jeweils in einen Kontext zu setzen um den Jugendlichen zu helfen einen Werterahmen zu konstruieren an dem sie sich orientieren können.",
+            quote1: "Ja, es ist normalisiert in unserer Gesellschaft Alkohol zu trinken. Ich trinke auch manchmal Alkohol. Das ändert nichts daran, dass es ein Suchtmittel ist.",
+            quote2: "Du bist nicht der erste Jugendliche und sicher auch nicht der Letzte, der in deinem Alter Alkohol trinkt. Alkohol ist nun mal reizvoll und der Staat profitiert nunmal auch davon, wenn wir bereits jung anfangen zu trinken. Die Jugend ist nun mal ein Alter wo man sich ausprobieren mag und wenn du deine Eltern fragst werden die dir womöglich auch ein paar Geschichten erzählen können aus ihren Jugendjahren. Sei dir halt bewusst, dass Alkohol abhängig machen kann und dass es dich besonders in deiner Entwicklung schädigen kann.",
+            text3: "So gern wir auch mehr Einfluss hätten. Die Realität ist nun einmal, dass Jugendliche saufen ob wir Alkohol nun an den Pranger stellen oder nicht. Gleichzeitig ist es doch scheinheilig. Welcher Erwachsene hat nicht schon mal einen Vollrausch gehabt?",
+            text4: "Wichtig bei dem wertungsfreien Zugang ist es aufzupassen, Alkoholkonsum nicht nebenbei zu verherrlichen. Es gibt einen kleinen, aber wichtigen Unterschied zwischen '...und wenn du deine Eltern fragst werden die dir womöglich auch ein paar Geschichten erzählen können aus ihren Jugendjahren.' gegenüber '...und wenn du deine Eltern fragst werden die dir womöglich auch ein paar lustige Geschichten erzählen können aus ihren Jugendjahren.' In der einen Version stellen wir klar, dass der gesellschaftliche Umgang mit Alkohol ein scheinheiliger ist, in der zweiten Version machen wir diesen Umgang erstrebenswert und idealisieren ihn - wer will schließlich nicht etwas lustiges erleben und erzählen können?"
+        },
+        phones: {
+            title: "Handys im Unterricht nutzen",
+            text: "Das Handy im Unterricht oder in Workshops einzusetzen ist ein zweischneidiges Schwert. Auf der einen Seite wird es zumeist sehr positiv von Schüler:innen aufgenommen, die Methoden, die ich im Folgenden beschreibe, sind interaktiv und führen meist zu einer großen Beteiligung. Gleichzeitig fesselt das Handy die Aufmerksamkeit der Schüler:innen. Daher ist es besonders wichtig, dass wir als Pädagog:innen bewusst und klar sagen, wann das Handy von allen weggelegt werden soll und wann sie es wieder in die Hand nehmen dürfen. Generell gilt: Sobald sie das Handy in der Hand haben, hören 80% euch nicht mehr zu. Immer wenn ihr die volle Aufmerksamkeit wollt, lasst sie die Handys weglegen."
+        },
+        party: {
+            title: "Gemeinsam auf einer fiktiven Party",
+            text1: "Mit einer Gruppe in der Größe einer Schulklasse lässt sich mit C-alcohol gut visualisieren, wie der gleiche Alkohol unterschiedlich auf verschiedene Körper wirkt.",
+            instructions: "`Stellt euch vor, ihr geht mit euren Freunden feiern. Im Laufe des Abends trinkt ihr was. Was wär denn so normal an einem gemütlichen Abend zu trinken?` Wartet auf Antworten, die müssen nicht elaboriert sein. `Wirkt Alkohol auf alle Menschen gleich?` Wahrscheinlich sagt irgendwer nein, dann geht ihr kurz mit ihnen durch, dass eben die Wassermenge im Körper entscheidend ist und die von Größe, Alter, Gewicht und Geschlecht (weil statistischer Körperfettanteil) abhängig ist. Dann zeigt ihr ihnen den QR-code und sagt ihnen, dass sie den scannen sollen. `Stellt mal alle eure Eigenschaften ein und dann fügt die Getränke hinzu, die ihr gerade aufgezählt habt: x,y,z` Gebt ihnen dann ein bisschen Zeit damit herumzuspielen - wenn man auf die verschiedenen Bereiche im Diagramm drückt, bekommt man eine Erklärung, was bei welchem Promill- Gehalt passiert.Zum Abschluss eskaliert ruhig mit ihnen indem ihr euch in ein virtuelles Koma schießt und schaut, wer zuerst die 3, 5 Promill erreicht, wenn ihr gleichzeitig die selben drinks `trinkt` (spoiler: es werden die kleinsten Mädchen sein, die `gewinnen`).",
+            note: "Wichtig: Sagt dazu, dass das kein Tool ist mit dem man tatsächlich tracken kann wieviel Promill man hat, da die Resorption auch viel vom Essen abhängt."
+        },
     }
 }
